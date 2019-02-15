1d800527b0b836f51cf5f61525bd2ed58e847de7
Your Name <dorian.l.wallace@gmail.com>
2019-02-15 16:55:10 -0500

    still working on delete, currently in the code I can post checkbox parameters

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -10,8 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static javax.swing.text.html.CSS.getAttribute;
 
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
@@ -23,16 +27,27 @@ public class ViewProfileServlet extends HttpServlet {
         }
         List<Ad> allAds = new ArrayList<>(DaoFactory.getAdsDao().all());
         List<Ad> myAds = new ArrayList<>();
+        Long currentId = null;
 
-        for(int i = 0; i < allAds.size(); i++){
-            if( currentUser.getId() == allAds.get(i).getUserId()){
-            //add allAds.get(i) to myAds array
+        for (int i = 0; i < allAds.size(); i++) {
+            if (currentUser.getId() == allAds.get(i).getUserId()) {
                 myAds.add(allAds.get(i));
+                currentId = allAds.get(i).getId();
+        request.setAttribute("name", currentId);
             }
-//                request.setAttribute("myTitle", allAds.get(i).getTitle());
-//                request.setAttribute("description", allAds.get(i).getDescription());
         }
         request.setAttribute("ads", myAds);
-                request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         String[] checkedAds = request.getParameterValues("checked");
+        System.out.println(Arrays.asList(checkedAds));
+
+//        DaoFactory.getAdsDao().findByTitle(ad.getTitle())
+//        System.out.println(request.getParameter());
+
     }
 }
+
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -3,24 +3,27 @@
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
-        <jsp:param name="title" value="Your Profile" />
+        <jsp:param name="title" value="Your Profile"/>
     </jsp:include>
 </head>
 <body>
-    <jsp:include page="/WEB-INF/partials/profile-navbar.jsp" />
+    <jsp:include page="/WEB-INF/partials/profile-navbar.jsp"/>
 
     <div class="container">
         <h1>Welcome, ${sessionScope.user.username}!</h1>
-                <p>Note: To make changes to an existing ad, click on the ad title...</p>
-                <button><a href="/ads/create">Create Another Ad</a></button>
-
-        <c:forEach var="ad" items="${ads}">
-            <div class="col-md-6">
-                <h2><a href="/edit-ad?selectedValue=${ad.title}">${ad.title}</a></h2>
-                <p>${ad.description}</p>
-            </div>
-        </c:forEach>
+        <p>Note: To make changes to an existing ad, click on the ad title...</p>
+        <button><a href="/ads/create">Create Another Ad</a></button>
+        <form method="POST" action="/profile">
+            <c:forEach var="ad" items="${ads}">
+                <div class="col-md-6">
+                    <h2><a href="/edit-ad?selectedValue=${ad.title}">${ad.title}</a></h2>
+                    <p>${ad.description}</p>
+                    <input type="checkbox" name="checked" value=${ad.id}>
+                </div>
+            </c:forEach>
+            <button type="submit">Confirm deletion</button>
+        </form>
     </div>
-    <jsp:include page="/WEB-INF/partials/foot.jsp" />
+    <jsp:include page="/WEB-INF/partials/foot.jsp"/>
 </body>
 </html>
